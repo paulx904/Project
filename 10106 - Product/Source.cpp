c72758239d18e4a13f87a9ceaa10f0c4e992ba7f
paulx904 <37829775+paulx904@users.noreply.github.com>
2020-05-05 02:01:10 +0800

    Add files via upload

---
+++ 10106 - Product/Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+#define MAX_SIZE 1000
+
+int main()
+{
+	int integer1[MAX_SIZE], integer2[MAX_SIZE];
+	char string1[MAX_SIZE], string2[MAX_SIZE];
+
+
+}
+
+
